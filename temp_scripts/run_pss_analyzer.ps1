6ea8d250b9437bfe9deade8adc3614988a2e4f65
ichimbogdancristian <ichimbogdancristian@gmail.com>
2026-02-05 15:47:26 +0200

    style: Improve formatting for readability in PSScriptAnalyzer script

--- temp_scripts/run_pss_analyzer.ps1
+++ temp_scripts/run_pss_analyzer.ps1
@@ -4,8 +4,8 @@ if (-not (Get-Module -ListAvailable -Name PSScriptAnalyzer)) {
     Install-Module PSScriptAnalyzer -Force -Scope CurrentUser -AllowClobber -Confirm:$false
 }
 Import-Module PSScriptAnalyzer -Force
-$results = Invoke-ScriptAnalyzer -Path . -Recurse -Settings '.\PSScriptAnalyzerSettings.psd1' -Severity Error,Warning -Force
-$out = $results | Select-Object @{n='File';e={$_.ScriptName}},Line,RuleName,Severity,Message
+$results = Invoke-ScriptAnalyzer -Path . -Recurse -Settings '.\PSScriptAnalyzerSettings.psd1' -Severity Error, Warning -Force
+$out = $results | Select-Object @{n = 'File'; e = { $_.ScriptName } }, Line, RuleName, Severity, Message
 $out | ConvertTo-Json -Depth 5 | Out-File -FilePath '.\temp_files\analysis\psscriptanalyzer-results.json' -Encoding UTF8 -Force
 if ($results.Count -gt 0) {
     $out | Format-Table -AutoSize
